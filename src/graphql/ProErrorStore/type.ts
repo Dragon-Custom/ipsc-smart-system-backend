0ea7046ed89f736ae3908bce05a3d373498d7bdd
jkh675 <rluo2018@gmail.com>
2024-04-28 22:05:06 +0800

    impl proErrorStore object

--- src/graphql/ProErrorStore/type.ts
+++ src/graphql/ProErrorStore/type.ts
@@ -3,15 +3,13 @@ import { objectType } from "nexus";
 export const ProErrorStoreObject = objectType({
 	name: "ProErrorStore",
 	definition(t) {
-		t.int("id");
+		t.implements("Node");
 		t.int("count");
 		t.field("proError", {
 			type: "ProErrorObject",
 		});
-		t.int("proErrorObjectId");
 		t.field("score", {
 			type: "Score",
 		});
-		t.int("scoreId");
 	},
 });
