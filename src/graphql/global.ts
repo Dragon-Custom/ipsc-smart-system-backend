8b17765a97ace8775912023b04a91b3bc6ff577b
jkh675 <rluo2018@gmail.com>
2024-05-01 17:30:06 +0800

    update comment

--- src/graphql/global.ts
+++ src/graphql/global.ts
@@ -2,6 +2,11 @@ import { interfaceType } from "nexus";
 
 export const NodeObject = interfaceType({
 	name: "Node",
+	resolveType(root) { //idk what the fuck are this thing doing
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		//@ts-expect-error
+		return root.__typename;
+	},
 	definition(t) {
 		t.nonNull.int("id");
 	},
