2d179b87303aa2a43f850bbc66eb56b0993a66c3
jkh675 <rluo2018@gmail.com>
2024-06-02 11:10:27 +0800

    setup the gql object

---
+++ src/graphql/Elo/type.ts
@@ -0,0 +1,15 @@
+import { objectType } from "nexus";
+
+export const EloType = objectType({
+	name: "Elo",
+	definition(t) {
+		t.implements("Node");
+		t.nonNull.field("shooter", {
+			type: "Shooter",
+		});
+		t.nonNull.int("shooterId");
+		t.nonNull.float("elo");
+		t.nonNull.dateTime("updatedAt");
+		t.nonNull.dateTime("createAt");
+	},
+});
