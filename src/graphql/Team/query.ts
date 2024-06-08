32328bf0ecd9cc7a1c7a2a3201e3835a71cfecae
normalid <rluo2018@gmail.com>
2024-06-08 21:23:03 +0800

    Impl "team" query

---
+++ src/graphql/Team/query.ts
@@ -0,0 +1,25 @@
+import { extendType, nonNull } from "nexus";
+import { LogLevel } from "../../context";
+
+
+const LOG_CAT = "Team";
+export const TeamQuery = extendType({
+	type: "Query",
+	definition(t) {
+		t.field("team", {
+			type: "Team",
+			args: {
+				id: nonNull("Int"),
+			},
+			resolve(root, args, ctx) {
+				ctx.log(LogLevel.INFO, `Querying team by id: ${args.id}`, LOG_CAT);
+				return ctx.prisma.team.findUnique({
+					where: {
+						id: args.id,
+					},
+					...ctx.select,
+				});
+			},
+		});
+	},
+});
