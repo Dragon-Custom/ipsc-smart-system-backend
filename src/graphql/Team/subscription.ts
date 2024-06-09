264f20e363f79f5b9f9ba3c93ddc4c45179d8ceb
normalid <rluo2018@gmail.com>
2024-06-09 10:42:19 +0800

    Implemented

---
+++ src/graphql/Team/subscription.ts
@@ -0,0 +1,30 @@
+import { extendType } from "nexus";
+import { LogLevel, subscribe } from "../../context";
+const LOG_CAT = "Team";
+
+export const ShooterSubscription = extendType({
+	type: "Subscription",
+	definition(t) {
+		t.boolean("teamsChange", {
+			subscribe(src, args, ctx) {
+				return (async function* () {
+					while (true) {
+						await new Promise(res => subscribe("Team", [
+							"create",
+							"createMany",
+							"update",
+							"updateMany",
+							"delete",
+							"deleteMany",
+						], res));
+						ctx.log(LogLevel.INFO, "Team subscription triggered", LOG_CAT);
+						yield true;
+					}
+				})();
+			},
+			resolve(eventData) {
+				return eventData;
+			},
+		});
+	},
+});
