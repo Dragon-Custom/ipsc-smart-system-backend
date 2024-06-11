77ff2e734086685c00f60219a5850d74d0f13da5
normalid <52519933+jkh675@users.noreply.github.com>
2024-06-11 16:28:35 +0800

    features #22/optimize field (#23)
    * rename shooter.elo -> elos
    
    * implment the refactor

--- src/graphql/Shooter/type.ts
+++ src/graphql/Shooter/type.ts
@@ -30,11 +30,59 @@ export const ShooterType = objectType({
 		t.nullable.list.field("rankings", {
 			type: "Ranking",
 		});
+		t.nonNull.int("ranking", {
+			resolve: async (src, args, ctx) => {
+				return (await ctx.prisma.ranking.findFirst({
+					where: {
+						id: src.id,
+					},
+					orderBy: {
+						tick: "desc",
+					},
+					select: {
+						rank: true,
+					},
+					take: 1,
+				}))?.rank || 0;
+			},
+		});
 		t.nullable.list.field("ratings", {
 			type: "Rating",
 		});
-		t.nullable.list.field("elo", {
+		t.nonNull.float("rating", {
+			resolve: async (src, args, ctx) => {
+				return (await ctx.prisma.rating.findFirst({
+					where: {
+						shooterId: src.id,
+					},
+					orderBy: {
+						tick: "desc",
+					},
+					select: {
+						rating: true,
+					},
+					take: 1,
+				}))?.rating || 0;
+			},
+		});
+		t.nullable.list.field("elos", {
 			type: "Elo",
+		});
+		t.nonNull.float("elo", {
+			resolve: async (src, args, ctx) => {
+				return (await ctx.prisma.elo.findFirst({
+					where: {
+						shooterId: src.id,
+					},
+					orderBy: {
+						tick: "desc",
+					},
+					select: {
+						elo: true,
+					},
+					take: 1,
+				}))?.elo || 0;
+			},
 		});
 		t.nullable.field("team", {
 			type: "Team",
